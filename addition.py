b9057e71bcc769c6f67d0fc53deb45be5423bd0d
Marshall Demars <marshall.demars@mths.ca>
2022-10-11 14:53:24 +0000

    new file

---
+++ addition.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Marshall Demars
+# Created on: October 2022
+# This program calculates the sum of two numbers
+#    with user input
+
+import math
+
+
+def main():
+    # this function calculates sum of two number
+
+    # input
+    integer_one = int(input("Enter integer one: "))
+    integer_two = int(input("Enter integer two: "))
+
+    # process
+    sum = integer_one + integer_two
+
+    # output
+    print("\nSum is {}." .format(sum))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
